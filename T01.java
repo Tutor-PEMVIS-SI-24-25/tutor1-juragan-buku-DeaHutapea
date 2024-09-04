dffd673324c41b3de6f341fab5de177495883d57
DeaHutapea <deahutapea99@gmail.com>
2024-09-04 21:27:12 +0700

    sudah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24053 - Dea Anggreany Hutapea
+// 12S24053 - Dea Anggreany Hutapea
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println(nama);
+    }
+}
 
